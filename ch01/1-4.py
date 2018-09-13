38a0fb095da967f7794973855d982002210f38ff
virus <yystopf@163.com>
2018-09-13 16:40:38 +0800

    查找最大或最小的 N 个元素

---
+++ ch01/1-4.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2018/9/12
+# @Author : virus 
+# @File : 1-4.py 
+# @Desp : 查找最大或最小的 N 个元素
+
+import heapq
+nums = [1,8,2,23,7,-4,18,23,42,37,2]
+
+print(heapq.nlargest(3,nums))
+print(heapq.nsmallest(3,nums))
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+cheap = heapq.nsmallest(3,portfolio, key=lambda s:s['price'])
+expensive = heapq.nlargest(3,portfolio, key=lambda  s:s['price'])
+
+print(cheap)
+print(expensive)
