6a1f2950fa9b41d100dd0d5abdf5080e7f5f5cb5
virus <yystopf@163.com>
2018-09-19 16:43:03 +0800

    删除序列相同元素并保持顺序

---
+++ ch01/1-10.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2018/9/14
+# @Author : virus 
+# @File : 1-10.py 
+# @Desp : 删除序列相同元素并保持顺序
+
+
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+
+a = [1,5,2,1,9,1,5,10]
+print(list(dedupe(a)))
+
+
+def dedupe(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+a = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+
+print(list(dedupe(a, key=lambda d: (d['x'],d['y']))))
+print(list(dedupe(a, key=lambda d: (d['x']))))
+
+
+
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+
+print(set(a))
